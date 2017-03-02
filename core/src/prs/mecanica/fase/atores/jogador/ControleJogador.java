ef672b6b4da5037dcf82348d9442c183601ff3a0
Santiago Lovera <santiago.lovera@gmail.com>
2017-03-02 18:58:35 -0300

    limpeza

--- core/src/prs/mecanica/fase/atores/jogador/ControleJogador.java
+++ core/src/prs/mecanica/fase/atores/jogador/ControleJogador.java
@@ -2,8 +2,6 @@ package prs.mecanica.fase.atores.jogador;
 
 import prs.mecanica.fase.atores.entidades.direcao.Direcoes;
 
-
-//todo diminuir nomes dos metodos
 interface ControleJogador {
 
     void initMovTecla(Direcoes direcao);
