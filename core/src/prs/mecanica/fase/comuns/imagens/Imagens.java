d10b8c1bdce5d2cf8c10e2740c26066ae91f361d
Santiago Lovera <santiago.lovera@gmail.com>
2017-03-04 13:39:02 -0300

    adicionando imagens ao enum imagens

--- core/src/prs/mecanica/fase/comuns/imagens/Imagens.java
+++ core/src/prs/mecanica/fase/comuns/imagens/Imagens.java
@@ -2,10 +2,19 @@ package prs.mecanica.fase.comuns.imagens;
 
 public enum Imagens {
 
-    PERSONAGEM_CIMA("pcima"),
+    PERSONAGEM_CIMA ("pcima"),
     PERSONAGEM_BAIXO("pbaixo"),
-    PERSONAGEM_ESQ("pesquerda"),
-    PERSONAGEM_DIR("pdireita");
+    PERSONAGEM_ESQ  ("pesquerda"),
+    PERSONAGEM_DIR  ("pdireita"),
+
+    BTN_DIR    ("btn_direita"),
+    BTN_ESQ    ("btn_esquerda"),
+    BTN_CIMA   ("btn_cima"),
+    BTN_BAIXO  ("btn_baixo"),
+    BTN_DIR_SUP("btn_direitasuperior"),
+    BTN_DIR_INF("btn_direitainferior"),
+    BTN_ESQ_SUP("btn_esquerdasuperior"),
+    BTN_ESQ_INF("btn_esquerdainferior");
 
     private String nomeArquivo;
 
